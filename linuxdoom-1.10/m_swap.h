ce71b82a3676583b52301c24a54416a63588c598
Matthew Yang <matthew.yang204@gmail.com>
2025-11-27 07:18:44 -0800

    These are unsigned, not regular

--- linuxdoom-1.10/m_swap.h
+++ linuxdoom-1.10/m_swap.h
@@ -32,8 +32,8 @@
 // Endianess handling.
 // WAD files are stored little endian.
 #ifdef __BIG_ENDIAN__
-short	SwapSHORT(unsigned short x);
-long	SwapLONG(unsigned long x);
+unsigned short	SwapSHORT(unsigned short x);
+unsigned long	SwapLONG(unsigned long x);
 #define SHORT(x)	((short)SwapSHORT((unsigned short) (x)))
 #define LONG(x)         ((long)SwapLONG((unsigned long) (x)))
 #else
